9ba12251eab051124a5ac7577cb1cf92068364bf
hkhcoder <hkhcoder@gmail.com>
2025-08-22 11:51:47 +0530

    e2.small

--- main.tf
+++ main.tf
@@ -88,7 +88,7 @@ resource "google_container_node_pool" "primary_nodes" {
   node_count = 1
 
   node_config {
-    machine_type = "e2-micro"
+    machine_type = "e2-small"
     disk_size_gb = 10
     disk_type    = "pd-standard"
 
